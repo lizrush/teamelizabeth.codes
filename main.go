98f4584b43a490bfcdb79643f78c70d750db4c8c
liz rush <liz@lizmrush.com>
2014-08-29 22:15:42 -0700

    add comments

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
-package main
+package main // name of the follow package below
 
-import "github.com/go-martini/martini"
-import "github.com/martini-contrib/render"
-import "teamelizabeth.codes/string"
+import "github.com/go-martini/martini" // framework
+import "github.com/martini-contrib/render" // rendering package to make things like erbs
+import "teamelizabeth.codes/string" // i wrote this package
 
-func main() {
-  m := martini.Classic()
-  m.Use(render.Renderer(render.Options{
-    Directory: "templates",
-    Layout: "layout",
+func main() { // when we run "go run main.go" it will call this method
+  m := martini.Classic() // sets up the martini framework to handle requests
+  m.Use(render.Renderer(render.Options{ // tells martini to use the render package Renderer with the following options
+    Directory: "templates", // for our pages that render into the yeild of the template
+    Layout: "layout", // this is the main layout into which other pages render
     Extensions: []string{".tmpl", ".html"}, // Specify extensions to load for templates.
   }))
 
@@ -16,12 +16,12 @@ func main() {
     return "THIS SITE IS FOR ELIZABETH EYES ONLY!"
   })
 
-  m.Get("/hello", func(r render.Render) {
-    r.HTML(200, "hello", "elizabeth")
+  m.Get("/hello", func(r render.Render) { //uses the render package
+    r.HTML(200, "hello", "elizabeth") // specifies http code, template, content for the {{.}}
   })
 
   m.Get("/olleh", func(r render.Render) {
-    r.HTML(200, "hello", reverse_string.Reverse("elizabeth"))
+    r.HTML(200, "hello", reverse_string.Reverse("elizabeth")) // calls my reverse funtion before passing to template
     })
 
   m.Run()
--- string/reverse_string.go
+++ string/reverse_string.go
@@ -1,10 +1,13 @@
-package reverse_string
+package reverse_string // name of package to be called, ie "reverse_string.Reverse("stuff")
 
-func Reverse(s string) string {
-	b := []byte(s)
-	for i := 0; i < len(b)/2; i++ {
-		j := len(b)-i-1
-		b[i], b[j] = b[j], b[i]
+func Reverse(s string) string { // name of funtion, type of thing passed in which
+                                // will be referenced in the function as 's',
+                                // then i think the type of the output?
+	b := []byte(s) // the bytes of the string- will break on some unicode chars
+	for i := 0; i < len(b)/2; i++ { // for loop sorta like js, with incrementor,
+                                  // when i < 1/2 oflength of string in bytes
+		j := len(b)-i-1 // black magic???
+		b[i], b[j] = b[j], b[i] // reverse
 	}
-	return string(b)
+	return string(b) //return with type specified
 }
